edab3e6bb60c70648dd560b4e8700bac387a4b1c
dag-H15 <dagihabtamu15@gmail.com>
2025-02-19 12:38:28 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GroupMembers from "./GroupMembers.jsx"; 
+const App = () => {
+  return (
+    <div>
+      <h1>name of group members</h1>
+      <GroupMembers /> 
+    </div>
+  );
+};
+
+export default App;
